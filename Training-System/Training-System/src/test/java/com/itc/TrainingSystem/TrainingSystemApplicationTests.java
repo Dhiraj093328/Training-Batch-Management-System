8c8a2a7bed06d27481f33062c4122ecdb2eb589c
Dhiraj093328 <162727053+Dhiraj093328@users.noreply.github.com>
2026-04-29 20:37:08 +0530

    project

---
+++ Training-System/Training-System/src/test/java/com/itc/TrainingSystem/TrainingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itc.TrainingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
